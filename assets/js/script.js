d33b0edaedf647942d5389cd71df0c20f4ccbdc8
NandhiniKumar <nandhusuji11@gmail.com>
2024-10-26 22:56:41 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,6 @@ document.getElementById("downloadResume").addEventListener("click", function()
  {
     const link = document.createElement("a");
     link.href = "assets/Nandhinikumarresume.docx"; // Update this path to your resume's location
-    link.download = "Nandhinikumarresume.pdf"; // Name of the downloaded file
+    link.download = "Nandhinikumarresume.docx"; // Name of the downloaded file
     link.click();
 });
